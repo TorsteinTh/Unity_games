d1c12abbe1f90537ac77517c92ecf4d6aa8b1a95
Torstein Thomassen <torstein1996@hotmail.com>
2019-04-07 23:03:06 +0200

    added Space_Rocket

---
+++ Rocket space/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundry
+{
+    public float xMin, xMax, zMin, zMax;
+
+
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundry boundry;
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);    
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movment = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().velocity = movment * speed;
+
+        GetComponent<Rigidbody>().position = new Vector3( 
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundry.xMin, boundry.xMax),
+            0.0f, 
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundry.zMin, boundry.zMax)
+        );
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f,0.0f,GetComponent<Rigidbody>().velocity.x * -tilt);
+    }
+}
